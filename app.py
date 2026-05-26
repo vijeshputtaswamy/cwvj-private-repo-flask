a8bdf4676054ea4c556122657e10d61b9652470b
Vijesh Puttaswamy <vijesh.hs@gmail.com>
2026-05-26 22:02:05 +1200

    feature(ui): update UI string

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.get("/")
 def hello():
     return jsonify(
         message="✨ Welcome to Cloud with VarJosh ✨",
-        tip="Built with Flask, shipped by Jenkins, running in Docker."
+        tip="Built with Flask, shipped by Jenkins, running in Docker.",
+        UI="This is my new UI. Vijesh here"
     )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
